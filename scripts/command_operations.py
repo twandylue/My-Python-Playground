e0c28c001efa6a3ac562055c2f27092e635337c3
AndyLu <laserlue@gmail.com>
2022-11-27 01:27:22 +0800

    update command_operations.py

--- scripts/command_operations.py
+++ scripts/command_operations.py
@@ -4,6 +4,16 @@ import shutil
 from pprint import pprint
 
 command = "echo 'hello'"
-result = subprocess.run(command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# WARNING: before Python 3.7:
+# result = subprocess.run(command.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result = subprocess.run(command.split(' '), capture_output=True)
 print(result.stdout)
 print(result.stderr)
+print(result.returncode)
+
+# multiple commands
+command2 = "echo a1; echo b2; ls"
+
+result2 = subprocess.run(command2, capture_output=True, shell=True)
+print(result2.stdout.decode())
+print(result.returncode)
