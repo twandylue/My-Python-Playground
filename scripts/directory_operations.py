442d0c0c10d78549ae4ef145bcd5897703e9ba5e
AndyLu <laserlue@gmail.com>
2022-11-27 09:36:26 +0800

    add cd in directory_operations.py

--- scripts/directory_operations.py
+++ scripts/directory_operations.py
@@ -7,11 +7,28 @@ my_cwd = os.getcwd();
 print(my_cwd)
 
 dir_list = os.listdir();
-for i in dir_list:
-    print(i)
+# for i in dir_list:
+#     print(i)
 
 print(dir_list)
 
-print(os.path.split(os.getcwd()))
+print('------ ### ------')
 
-shutil.copyfile('test.py', 'test2.py')
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+with cd("../"):
+    # subprocess.call("pwd")
+    dir_list = os.listdir();
+    print(dir_list)
+    print(os.getcwd())
+    subprocess.call("tree")
